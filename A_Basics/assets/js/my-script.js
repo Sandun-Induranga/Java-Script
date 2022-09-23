d76af391b087f15a021ce43b59a33565584173e8
Sandun-Induranga <sandutharu40@gmail.com>
2022-09-23 10:02:20 +0530

    Introduced variables for text input

--- A_Basics/assets/js/my-script.js
+++ A_Basics/assets/js/my-script.js
@@ -1,1 +1,62 @@
 console.log("Hello there I'm from the console")
+
+/* Variables */
+var x = 'Hello JS'; // Loosely typed - No type
+typeof x;
+
+/*
+*
+*   In Java
+*   String s = ""; // Type is strong
+*
+*   In JS
+*   var x = 10;
+*   x = 'Ten';
+*   x = true; // This is Dynamic Typing
+*   Case Sensitive
+*
+* */
+
+/* Troubles we have to face by creating variables
+*
+*   1
+*   var myState = false;
+*   var myState = true; // Legal ,Can duplicate
+*
+*   2
+*   var myTp = 119
+*   {
+*       console.log(myTp) // Legal
+*       var myAge = 100;
+*   }
+*   console.log(myAge); // This is also legal
+*
+*   console.log(newVar)  // No Error because the x defined bellow (Only with var keyword)
+*   newVar = 10
+*
+*
+*   // Hoisting - this is the special feature in var
+*
+*   console.log(x)
+*   newVar = 10
+*   console.log(x)  // No Error because the x defined bellow (Only with var keyword)
+*
+*
+*
+* ***************************************************** let ******************************************************
+*
+*   let myName = 'Dasun'
+*   let myName = 'Nimal' // Error
+*   console.log(myName)
+*
+*   {
+*       let myAge = 10;
+*   }
+*
+*   console.log(myAge) // Error  - Scope is considered
+*
+*   let also support for dynamic supporting
+*
+*   Not Supporting for Hoisting
+*
+* */
